c30693c3732efb73054f1944dd76847b5e54a5ed
Maciek <mstikowski94@gmail.com>
2020-08-04 19:11:15 +0200

    Updates

---
+++ precache-manifest.c27c80db8c7ac85c0991410a70eed316.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3092ccdf2a7582871a57665a3b0a556d",
+    "url": "/task-manager/index.html"
+  },
+  {
+    "revision": "466da3ff9b5b077f6fe2",
+    "url": "/task-manager/static/css/main.06a7e52a.chunk.css"
+  },
+  {
+    "revision": "81a660e029c9357361aa",
+    "url": "/task-manager/static/js/2.ae36ea60.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/task-manager/static/js/2.ae36ea60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "466da3ff9b5b077f6fe2",
+    "url": "/task-manager/static/js/main.b4cdbd62.chunk.js"
+  },
+  {
+    "revision": "6f879587e75cb206c02c",
+    "url": "/task-manager/static/js/runtime-main.97270499.js"
+  }
+]);
