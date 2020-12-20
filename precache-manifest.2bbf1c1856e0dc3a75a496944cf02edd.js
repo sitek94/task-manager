35dcc16ab73a7b7022ca3b9a52952cc444af176c
sitek94 <msitkowski94@gmail.com>
2020-12-20 19:23:59 +0100

    Updates

--- precache-manifest.2bbf1c1856e0dc3a75a496944cf02edd.js
+++ precache-manifest.2bbf1c1856e0dc3a75a496944cf02edd.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3092ccdf2a7582871a57665a3b0a556d",
+    "revision": "6940f8a4db7acbde77eb69c82dd6f2a3",
     "url": "/task-manager/index.html"
   },
   {
-    "revision": "466da3ff9b5b077f6fe2",
+    "revision": "84932270fa4cb54e8107",
     "url": "/task-manager/static/css/main.06a7e52a.chunk.css"
   },
   {
-    "revision": "81a660e029c9357361aa",
-    "url": "/task-manager/static/js/2.ae36ea60.chunk.js"
+    "revision": "51ca65494b53da373371",
+    "url": "/task-manager/static/js/2.b5ab2b52.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/task-manager/static/js/2.ae36ea60.chunk.js.LICENSE.txt"
+    "url": "/task-manager/static/js/2.b5ab2b52.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "466da3ff9b5b077f6fe2",
-    "url": "/task-manager/static/js/main.b4cdbd62.chunk.js"
+    "revision": "84932270fa4cb54e8107",
+    "url": "/task-manager/static/js/main.a578fa2f.chunk.js"
   },
   {
     "revision": "6f879587e75cb206c02c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/task-manager/precache-manifest.c27c80db8c7ac85c0991410a70eed316.js"
+  "/task-manager/precache-manifest.2bbf1c1856e0dc3a75a496944cf02edd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/task-manager/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
